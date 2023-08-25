90b281510e9b28273975e25e4c931d7bfead276e
Motechit <otikomosunmola@gmail.com>
2023-08-24 17:25:33 -0700

    Monty tasks

--- main.c
+++ main.c
@@ -7,12 +7,11 @@ char **op_toks = NULL;
 
 /**
  * main - the entry point for Monty Interp
- *
  * @argc: the count of arguments passed to the program
- * @argv: pointer to an array of char pointers to arguments
- *
+ * @argv: pointer to an array of char pointers to args
  * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
  */
+
 int main(int argc, char **argv)
 {
 	FILE *script_fd = NULL;
